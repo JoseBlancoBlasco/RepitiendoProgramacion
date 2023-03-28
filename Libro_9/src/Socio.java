1a1c08f9e03c5fa3e174a54ff581c29fac033a16
JoseBlancoBlasco <jose.blanco.blasco@gmail.com>
2023-03-28 21:21:23 +0200

    Add - Actividades Comparable y Comparator

---
+++ Libro_9/src/Socio.java
@@ -0,0 +1,45 @@
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+
+public class Socio implements Comparable {
+    
+    int id;
+    String nombre;
+    LocalDate fechaNacimiento;
+
+    public Socio(int id, String nombre, String fechaNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-mm-yyyy");
+        this.fechaNacimiento = LocalDate.parse(fechaNacimiento, f);
+    }
+    
+    int edad(){
+        return (int) fechaNacimiento.until(LocalDate.now(),ChronoUnit.YEARS);
+    }    
+
+    @Override
+    public int compareTo(Object t) {
+        int resultado;
+        Socio otroSocio = (Socio) t;
+        if (id < otroSocio.id) {
+            resultado = -1;
+        }else if (id>otroSocio.id) {
+            resultado = 1;
+        }else{
+            resultado = 0;
+        }
+        return resultado;
+    }
+
+    @Override
+    public String toString() {
+        return "Socio{" + "Id=" + id + ", Nombre=" + nombre + ", Edad=" + edad() + '}';
+    }
+    
+    
+    
+}
